8423a11125ba08a264f152736c4c0ddce3693605
Ahmerothi <ahmer.othi@ontariotechu.net>
2024-11-05 23:43:21 -0500

    getting the code for assignment 1

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-/* Installed third party packages  */
+/* Installed third-party packages */
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+// Import routes
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 let app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,29 +24,27 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
+// Routes
 app.use('/', indexRouter); // localhost:3000
-app.use('/users', usersRouter);// localhost:3000/users
+app.use('/users', usersRouter); // localhost:3000/users
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
-  res.render('error',
-    {
-      title:"Error"
-    }
-  );
+  res.render('error', {
+    title: "Error"
+  });
 });
 
-
-
 module.exports = app;
+allowedNodeEnvironmentFlags
--- routes/index.js
+++ routes/index.js
@@ -2,43 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { 
-    title: 'Home',
-    
-   });
-});
-/* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { 
-    title: 'Home',
-    
-   });
-});
-/* GET Aboutme page. */
-router.get('/about', function(req, res, next) {
-  res.render('index', { 
-    title: 'About me',
-    
-   });
-});
-/* GET projects page. */
-router.get('/projects', function(req, res, next) {
-  res.render('index', { 
-    title: 'Projects',
-    
-   });
-});
-/* GET contact page. */
-router.get('/contact', function(req, res, next) {
-  res.render('index', { 
-    title: 'Contact me',
-    
-   });
+router.get('/index', function(req, res) {
+  res.render('index', { title: 'Home' });
 });
 
+/* GET About Me page. */
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'About Me' });
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res) {
+  res.render('projects', { title: 'Projects' });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'Contact Me' });
+});
 
 module.exports = router;
-/*
-MVC --> Model view controller
-*/
